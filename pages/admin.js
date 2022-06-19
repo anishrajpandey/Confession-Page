74daee30328bfd1718d16c56cbda4d36055cd2a1
Anish949 <74363280+Anish949@users.noreply.github.com>
2022-06-19 09:47:04 +0545

    updates

--- pages/admin.js
+++ pages/admin.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import Image from "next/image";
 import styles from "./../styles/admin.module.css";
 
-const Admin = ({ apiEndpoint, updateApiEndpoint,at }) => {
+const Admin = ({ apiEndpoint, updateApiEndpoint, at }) => {
   const [PostList, setPostList] = useState([]);
   const fetchData = async () => {
     let data = await fetch(apiEndpoint);
@@ -15,68 +15,69 @@ const Admin = ({ apiEndpoint, updateApiEndpoint,at }) => {
   });
   const postToInstagram = async (url, caption = "") => {
     //CREATING MEAIA CONTAINER
-    console.log(url)
-    let media = await fetch(`https://graph.facebook.com/v14.0/17841451771977639/media?image_url=${url}&caption=${caption}&access_token=${at}`, {
-      method:"POST"
-    })
-    let {id} =await media.json(); 
-    console.log(id)  
-
+    console.log(url);
+    let media = await fetch(
+      `https://graph.facebook.com/v14.0/17841451771977639/media?image_url=${url}&caption=${caption}&access_token=${at}`,
+      {
+        method: "POST",
+      }
+    );
+    let { id } = await media.json();
+    console.log(id);
 
     //posting with media_id
-    await fetch(`https://graph.facebook.com/v14.0/17841451771977639/media_publish?creation_id=${id}&access_token=${at}`,{method:'POST'})
-  }
-  const handleAccept = async(e) => {
+    await fetch(
+      `https://graph.facebook.com/v14.0/17841451771977639/media_publish?creation_id=${id}&access_token=${at}`,
+      { method: "POST" }
+    );
+  };
+  const handleAccept = async (e) => {
     const id = e.target.parentElement.parentElement.id;
 
-   
     let data = await fetch(updateApiEndpoint, {
       method: "POST",
-      body: JSON.stringify({id: id ,shouldReturnUrlAsAResponse:true}),
+      body: JSON.stringify({ id: id, shouldReturnUrlAsAResponse: true }),
       headers: {
         "Content-Type": "application/json",
       },
     });
-    let { url} = await data.json();
-    postToInstagram(url)
-    console.log("POSTED TO INSTAGRAM")
-
+    let { url } = await data.json();
+    postToInstagram(url);
+    console.log("POSTED TO INSTAGRAM");
   };
-  
+
   const handleReject = async (e) => {
+    e.target.parentElement.parentElement.style.display = "none";
     const id = e.target.parentElement.parentElement.id;
 
     let res = await fetch(updateApiEndpoint, {
       method: "POST",
-      body: JSON.stringify({id: id }),
+      body: JSON.stringify({ id: id }),
       headers: {
         "Content-Type": "application/json",
       },
     });
-    console.log("REJECTED")
- 
+    console.log("REJECTED");
   };
   return (
     <main>
       {PostList.map((elem) => {
-       
         return (
-          
-         elem.isPending 
-          && 
-          <div key={elem._id} id={elem._id}>
-            <Image
-              src={elem.imageURL}
-              width={300}
-              height={300}
-              alt="Cannot Load Image"
-            ></Image>
-            <span> {elem.isPending ? "True" : "False"}</span>
-            <div className={styles.buttonArea}>
-              <button onClick={handleAccept}>Accept</button>
-              <button onClick={handleReject}>Reject</button>
+          elem.isPending && (
+            <div key={elem._id} id={elem._id}>
+              <Image
+                src={elem.imageURL}
+                width={300}
+                height={300}
+                alt="Cannot Load Image"
+              ></Image>
+              <span> {elem.isPending ? "True" : "False"}</span>
+              <div className={styles.buttonArea}>
+                <button onClick={handleAccept}>Accept</button>
+                <button onClick={handleReject}>Reject</button>
+              </div>
             </div>
-          </div>
+          )
         );
       })}
     </main>
@@ -89,7 +90,7 @@ export async function getServerSideProps() {
     props: {
       apiEndpoint: process.env.POST_API_ENDPOINT,
       updateApiEndpoint: process.env.UPDATE_API_ENDPOINT,
-      at:process.env.ACCESS_TOKEN
+      at: process.env.ACCESS_TOKEN,
     },
   };
 }
